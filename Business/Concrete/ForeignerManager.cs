91765f92d0d1bb07b36a043194efcab2c2a070a7
Hayri Taha ÇATAR <hayritahacatar@gmail.com>
2024-02-08 04:52:40 +0300

    Kodlama.io 2024 Yazılım Geliştirici Yetiştirme Kampı (C#) 1.Gün Ödev 2 - Maske Takip sistemine eğitimde bahsedilen eczaneden satış işlevi de eklendi.

---
+++ Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+
+namespace Business.Concrete
+{
+    public class ForeignerManager : IApplicantService
+    {
+        //Sonarqube
+        public void ApplyForMask(Person person)
+        {
+            
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            return person.NationalIdentity == 0;
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+    }
+}
---
+++ Business/Concrete/PharmacyManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PharmacyManager : ISellerService
+    {
+        private IApplicantService _applicantService;
+
+        public PharmacyManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+
+        }
+
+        public void SellMask(Person person)
+        {
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske satıldı");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske SATILAMADI");
+            }
+        }
+    }
+}
